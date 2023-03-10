023a34a0e7e672217753ad98d7f42c982f6f047e
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-10 10:30:08 +0530

    navbar and hero

---
+++ app/page.js
@@ -0,0 +1,29 @@
+/* eslint-disable quotes */
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
+
+const Page = () => (
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default Page;
